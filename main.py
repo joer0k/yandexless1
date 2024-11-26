30408532b80ce156c855b95b973c34ecf87ffd95
joer0k <ryabokon.niki08@gmail.com>
2024-11-26 18:24:30 +0300

    first task completed

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-
-
+import io
+import sys
+from PyQt6 import uic
+from random import randint
+from PyQt6.QtGui import QColor, QPainter
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QMainWindow
 
 template = '''<?xml version="1.0" encoding="UTF-8"?>
 <ui version="4.0">
@@ -47,3 +52,36 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
  <connections/>
 </ui>
 '''
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        f = io.StringIO(template)
+        uic.loadUi(f, self)
+        self.do_paint = False
+        self.btn_create_circle.clicked.connect(self.create)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+
+    def draw(self, qp):
+        qp.setBrush(QColor('yellow'))
+        r = randint(0, 250)
+        qp.drawEllipse(randint(0, 399 - r), randint(0, 561 - r), r, r)
+        self.do_paint = False
+
+    def create(self):
+        self.do_paint = True
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
